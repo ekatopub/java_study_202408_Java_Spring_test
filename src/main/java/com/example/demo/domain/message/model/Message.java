831f8946c29e2107087dd73bd910df02915e1cf1
ekatopub <ekatobiz@gmail.com>
2025-08-30 11:09:49 +0900

    CRUD1

---
+++ src/main/java/com/example/demo/domain/message/model/Message.java
@@ -0,0 +1,31 @@
+package com.example.demo.domain.message.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="message")
+
+//主キーの組み合わせを外部クラスで指定（複合キーなら必須）
+@IdClass(value=MessageKey.class)
+
+@Data
+//複合キーはSerializable必須（ないとコンパイルエラー）
+public class Message implements Serializable {
+
+  // ID
+  @Id // 主キーに対して付与
+  private int id;
+
+  // 投稿内容
+  private String text;
+
+  // 種別ID
+  private String kindId;
+}
---
+++ src/main/java/com/example/demo/repository/MessageDao.java
@@ -0,0 +1,24 @@
+package com.example.demo.repository;
+
+//import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Modifying;
+//import org.springframework.data.jpa.repository.Query;
+
+import com.example.demo.domain.message.model.Message;
+
+public interface MessageDao extends JpaRepository<Message, Integer> {
+	  /* JPQL定数 */
+	/*  final String JPQL_DELETE_MESSAGE_BY_KINDID = " DELETE "
+	    + " FROM Message m"
+	    + " WHERE m.kindId = :kindId "; */
+	  
+	  public Long deleteByKindId(String kindId); // JPA標準のメソッド
+	    
+	  /** 削除(種別ID指定) */
+//	  @Modifying
+//	  @Query(JPQL_DELETE_MESSAGE_BY_KINDID)
+//	  public int deleteByKindId(String kindId) throws DataAccessException;
+	  
+
+}
